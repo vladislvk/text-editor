659580767f838dd4ea9f5abbdd9e3fbc9d365b01
vladislvk <vladislavkim566@gmail.com>
2024-09-21 12:46:46 +0500

    feat:finished app

--- text-editor/ContentView.swift
+++ text-editor/ContentView.swift
@@ -2,17 +2,63 @@ import SwiftUI
 
 struct ContentView: View {
     @State var userText=""
-    
+    @State var capMode = 1
+
     var body: some View {
         VStack(content: {
-            Text(userText)
+            VStack(content: {
+                if capMode == 1{
+                    Text(userText.uppercased())
+                    
+                }
+                if capMode == 2{
+                    Text(userText.capitalized)
+                    
+                }
+                if capMode == 3{
+                    Text(userText.lowercased())
+                    
+                }
+            })
+            Spacer()
             TextField("Enter text",text: $userText)
                 .padding(20)
-            
-            HStack(content: {
-                CustomButtonView(title: "click", color: .red)
-                CustomButtonView(title: "click", color: .green)
-                CustomButtonView(title: "click", color: .blue)
+            VStack(content: {
+                Button(action: {
+                    let pasteBoard = UIPasteboard.general
+                    if capMode == 1{
+                        pasteBoard.string = userText.uppercased()
+                        
+                    }
+                    if capMode == 2{
+                        pasteBoard.string = userText.capitalized
+                        
+                    }
+                    if capMode == 3{
+                        pasteBoard.string = userText.lowercased()
+                        
+                    }
+                }, label: {
+                    CustomButtonView(title: "copy", color: .orange)
+                        
+                })
+                HStack(content: {
+                    Button(action: {
+                        capMode = 1
+                    }, label: {
+                        CustomButtonView(title: "click", color: .red)
+                    })
+                    Button(action: {
+                        capMode = 2
+                    }, label: {
+                        CustomButtonView(title: "click", color: .green)
+                    })
+                    Button(action: {
+                        capMode = 3
+                    }, label: {
+                        CustomButtonView(title: "click", color: .blue)
+                    })
+                })
             })
         })
     }
